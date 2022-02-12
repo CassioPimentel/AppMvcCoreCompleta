bb608aec7f1cb7ddc40502d7046860c3113248f8
Cassio Alves <cassiopimentel@odilonsantos.com>
2022-02-12 20:26:09 -0300

    Implementação da criação de produtos com imagens e melhorias nas views

--- src/Dev.App/Controllers/ProdutosController.cs
+++ src/Dev.App/Controllers/ProdutosController.cs
@@ -2,11 +2,13 @@
 using Dev.App.ViewModel;
 using Dev.Business.Interfaces;
 using Dev.Business.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Dev.App.Controllers
@@ -58,9 +60,18 @@ namespace Dev.App.Controllers
 
             if (!ModelState.IsValid) return View(produtoViewModel);
 
+            var imgPrefixo = Guid.NewGuid() + "_";
+
+            if (!await UploadArquivo(produtoViewModel.ImagemUpload, imgPrefixo))
+            {
+                return View(produtoViewModel);
+            }
+
+            produtoViewModel.Imagem = imgPrefixo + produtoViewModel.ImagemUpload.FileName;
+
             await _produtoRepository.Adicionar(_mapper.Map<Produto>(produtoViewModel));
 
-            return View(produtoViewModel);
+            return RedirectToAction("Index");
         }
 
         public async Task<IActionResult> Edit(Guid id)
@@ -81,11 +92,33 @@ namespace Dev.App.Controllers
         {
             if (id != produtoViewModel.Id) return NotFound();
 
-            if (ModelState.IsValid) return View(produtoViewModel);
+            var produtoAtualizacao = await ObterProduto(id);
 
-            await _produtoRepository.Atualizar(_mapper.Map<Produto>(produtoViewModel));
+            produtoViewModel.Fornecedor = produtoAtualizacao.Fornecedor;
+            produtoViewModel.Imagem = produtoAtualizacao.Imagem;
+
+            if (!ModelState.IsValid) return View(produtoViewModel);
+
+            if (produtoViewModel.ImagemUpload != null)
+            {
+                var imgPrefixo = Guid.NewGuid() + "_";
+
+                if (!await UploadArquivo(produtoViewModel.ImagemUpload, imgPrefixo))
+                {
+                    return View(produtoViewModel);
+                }
+
+                produtoAtualizacao.Imagem = imgPrefixo + produtoViewModel.ImagemUpload.FileName;
+            }
+
+            produtoAtualizacao.Nome = produtoViewModel.Nome;
+            produtoAtualizacao.Descricao = produtoViewModel.Descricao;
+            produtoAtualizacao.Valor = produtoViewModel.Valor;
+            produtoAtualizacao.Ativo = produtoViewModel.Ativo;
+
+            await _produtoRepository.Atualizar(_mapper.Map<Produto>(produtoAtualizacao));
             
-            return RedirectToAction("View");
+            return RedirectToAction("Index");
         }
 
         public async Task<IActionResult> Delete(Guid id)
@@ -127,6 +160,26 @@ namespace Dev.App.Controllers
             produto.Fornecedores = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
 
             return produto;
+        }
+
+        private async Task<bool> UploadArquivo(IFormFile arquivo, string imgPrefixo)
+        {
+            if (arquivo.Length <= 0) return false;
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/imagens", imgPrefixo + arquivo.FileName);
+
+            if (System.IO.File.Exists(path))
+            {
+                ModelState.AddModelError("ImagemUpload", "Já existe um arquivo com este nome.");
+                return false;
+            }
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await arquivo.CopyToAsync(stream);
+            }
+
+            return true;
         }
     }
 }
--- src/Dev.App/Models/EnderecoViewModel.cs
+++ src/Dev.App/Models/EnderecoViewModel.cs
@@ -17,6 +17,8 @@ namespace Dev.App.ViewModel
         [StringLength(50, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Numero { get; set; }
 
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(250, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Complemento { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
